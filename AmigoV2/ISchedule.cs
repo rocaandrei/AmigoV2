b9c22f5c319e7ba6c646098b210dfbaf249080aa
AndreiR <andrei.rocaa@gmail.com>
2018-10-29 20:41:53 +0200

    added next ISchedule that will creat my Engineer schedule.

---
+++ AmigoV2/ISchedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmigoV2
+{
+    public interface ISchedule
+    {
+        void ShuffleEngineers();
+        void AddShifts();
+        void ShowEngineers();
+    }
+}
